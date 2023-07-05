54a9f2e3984630c6d54e746450cfb1aac5f9b702
Shinei Shukla <109728779+shineishukla@users.noreply.github.com>
2023-07-05 22:09:12 +0530

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,74 @@
+//var, let, const
+
+var name = 'James';
+console.log(name);
+
+let age = 17;
+//after 1 year
+age = 18;
+console.log(age);
+
+let name1 = "James";
+console.log(name1);
+
+
+
+//alert function
+
+alert("I am an alert box");
+
+
+//concatenation
+
+console.log("Hakuna " + "Matata");
+
+
+console.log("Java" + 'Script');
+
+
+let x = "Java";
+let y = "Script";
+let z = x +y;//"Java" + "Script"
+console.log(z);
+
+
+console.log("2" + "2");
+
+
+
+//assignment operators
+
+let price = 50;
+let rate = 1.2;
+price*=rate; // price = price*rate;
+console.log(price);
+
+
+let score = 100;
+score+=10;
+console.log(score);
+
+
+let x1 = 15;
+x1+=5; // x1 = x1+5;
+console.log(x1);
+x1-=5; // x1 = x1-5;
+console.log(x1);
+x1*=5; // x1 = x1*5;
+console.log(x1);
+x1/=5; // x1 = x1/5;
+console.log(x1);
+x1%=5; // x = x1%5;
+console.log(x1);
+
+
+//increment and decrement operators
+
+let x2 = 5;
+console.log(++x2);
+
+let score1 = 100;
+score1--;
+score1--;
+console.log(score1);
+
---
+++ practice2.js
@@ -0,0 +1,65 @@
+//arithmetic operators
+
+let balls = 100;
+let boxes = 3;
+console.log(balls % boxes);
+
+let x = 2;
+let y = 5
+console.log(x**y);
+
+let x1 = 8;
+let y1 = 3;
+console.log(x1+y1);//addition
+console.log(x1-y1);//subtraction
+console.log(x1*y1);//multiplication
+console.log(x1/y1);//division
+
+
+//console function
+
+let x2 = 5; //whole number
+let y2 = 8.4; //decimal
+console.log(x2);
+console.log(y2);
+
+//string single quotes and double quotes
+
+let msg1 = "I am a string";
+let msg2 = 'I am a string as well';
+
+console.log(msg1);
+console.log(msg2);
+
+
+
+
+//template literals
+
+let name = "John";
+let text = `Welcome, ${name}`;
+console.log(text);
+
+
+let msg = `Hey! Are you sololearning?
+Isn't it awesome?`;
+console.log(msg);
+
+
+console.log(`I'm happy, she said "Yes"!`);
+
+
+let temp = `Hello, user!`;
+console.log(temp);
+
+
+
+
+//escape sequences
+
+console.log("One \nTwo \nThree");
+
+console.log("\t hey \t there");
+
+console.log('I\'m happy');
+console.log("She said \"Yes!\"");
