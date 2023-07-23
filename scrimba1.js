64c48b12a41535422c15ed70c6d496e7530a322e
shineishukla <shinei27shukla@gmail.com>
2023-07-23 22:47:40 +0530

    day 16 progress

---
+++ scrimba1.js
@@ -0,0 +1,22 @@
+// Day 16 of #100DaysOfCode
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+let myAge = 20;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
+
+
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+let bonusPoints = 50;
+console.log(bonusPoints);
+bonusPoints += 50;
+console.log(bonusPoints);
+bonusPoints -= 75;
+console.log(bonusPoints);
