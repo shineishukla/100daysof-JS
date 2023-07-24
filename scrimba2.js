3b27c638d5641b82d434557697f90ae6f8306d5e
shineishukla <shinei27shukla@gmail.com>
2023-07-24 20:21:07 +0530

    day 17 progress

---
+++ scrimba2.js
@@ -0,0 +1,35 @@
+/*Day 17 of #100DaysOfCode */
+
+// Concatenate two strings in a function, then log the result
+let name = "Linda";
+let greeting = "Hi there";
+function greetLinda() {
+    console.log(greeting + ", " + name + "!");
+}
+greetLinda();
+
+
+
+//variables practise
+let firstName = "Per"
+let lastName = "Borgen"
+let fullName = firstName + " " + lastName
+console.log(fullName)
+
+
+//incrementing/ decrementing
+let myPoints = 3
+function add3Points() {
+    myPoints += 3
+}
+function remove1Points() {
+    myPoints -= 1
+}
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+console.log(myPoints)
+
+
