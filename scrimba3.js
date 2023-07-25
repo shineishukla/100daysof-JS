8a3905df8a18e2cb7d9da5d11f092dd680ebc9c4
shineishukla <shinei27shukla@gmail.com>
2023-07-25 23:48:06 +0530

    day 18 progress

---
+++ scrimba3.js
@@ -0,0 +1,26 @@
+// Day 18 of #100DaysOfCode
+
+//check if the person is old enough to enter the club(21+)
+//if they are, welcome them in. otherwise, ask them to leave
+let age = 21;
+if (age<=20){
+    console.log("Sorry, you can't come in");
+}
+else{
+    console.log("Welcome, come on in!");
+}
+
+
+
+//check if the person is elegible for a birthday card from the King! (100+)
+let age1 = 100;
+if (age1<100){
+    console.log("Here is your birthday card from the King!");
+}
+else if(age1===100){
+    console.log("Not elegible for a birthday card from the King!");
+}
+else{
+    console.log("Error!");
+}
+
