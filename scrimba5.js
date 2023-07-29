3f55f9d808b7a45f086a7ddd9066c97a07c76ede
shineishukla <shinei27shukla@gmail.com>
2023-07-29 18:43:06 +0530

    day 20 progress

--- scrimba5.js
+++ scrimba5.js
@@ -1,6 +1,24 @@
+// Day 20 of #100DaysOfCode
+
 //add a click event listener to the button
 
 let button= document.getElementById("play-button");
 button.addEventListener("click",function(){
     console.log("button clicked");
-});   
+});
+
+
+
+
+//innerHTML 
+const container=document.getElementById("container");
+container.innerHTML="<button> BUY </button>";
+
+
+//truthy and falsy values
+const credits=0;
+if(credits){
+    console.log("Let's play!");
+}else{
+    console.log("Sorry, not enough credits!");
+}
