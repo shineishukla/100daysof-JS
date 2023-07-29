7b4a2bc885a99265684ee3c6b4a82a88d5c4bedf
shineishukla <shinei27shukla@gmail.com>
2023-07-29 14:20:37 +0530

    day 20

---
+++ scrimba5.js
@@ -0,0 +1,6 @@
+//add a click event listener to the button
+
+let button= document.getElementById("play-button");
+button.addEventListener("click",function(){
+    console.log("button clicked");
+});   
